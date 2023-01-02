f5314cfee4d144c15c05336e62916ae7906409d6
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2023-01-02 19:50:52 +0300

    update: add provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { TodoProvider } from "./contexts/TodoContext";
 
 function App() {
   return (
-    <>
+    <TodoProvider>
       <section className="todoapp">
         <Header />
         <Content />
       </section>
       <Footer />
-    </>
+    </TodoProvider>
   );
 }
 
